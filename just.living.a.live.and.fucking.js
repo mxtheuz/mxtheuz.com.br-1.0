a086a0e29d5c02411dfdba240c3c33d38515921a
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2023-08-05 13:28:50 -0300

    att: javascript update;

--- just.living.a.live.and.fucking.js
+++ just.living.a.live.and.fucking.js
@@ -1,7 +1,5 @@
 // 75d38c1d4a10b58ca617a7904a6967b5 (md5)
 
 console.log("eu vivo pra... pra que q eu vivo mesmo?");
-
-// year
 let year = new Date().getFullYear();
 let copy = document.querySelector("#year").innerHTML = " " + year + " "
