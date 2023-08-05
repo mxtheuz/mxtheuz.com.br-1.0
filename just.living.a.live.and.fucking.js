249b7fb1eac9bc95271f022e40b03851d640a285
Matheus Piccoli <mxtheuzchika@gmail.com>
2023-08-05 13:25:13 -0300

    First version of mxtheuz.com.br

---
+++ just.living.a.live.and.fucking.js
@@ -0,0 +1,7 @@
+// 75d38c1d4a10b58ca617a7904a6967b5 (md5)
+
+console.log("eu vivo pra... pra que q eu vivo mesmo?");
+
+// year
+let year = new Date().getFullYear();
+let copy = document.querySelector("#year").innerHTML = " " + year + " "
